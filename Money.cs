0c4570328941d8fc98a2d4bd96749ff39c8f554e
aawadall <aawadall@ualberta.ca>
2018-10-16 20:09:05 -0400

    Constructor to Money and Denomination

--- Money.cs
+++ Money.cs
@@ -2,8 +2,13 @@ namespace ATMClerk.Tester
 {
     public class Money
     {
+        public Money(double amount, Denomination denomination)
+        {
+            this.Amount = amount;
+            this.Denomination = denomination;
+        }
         
         public double Amount {get; private set;}
-        public Denomination denomination {get; private set;}
+        public Denomination Denomination {get; private set;}
     }
 }
