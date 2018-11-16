1920a0e1cfc233c5f4eec22966e2bf11be498508
Ahmed Awadallah <aawadall@ualberta.ca>
2018-11-16 12:30:44 -0500

    Defined Event Store interface

--- Events/IEventList.cs
+++ Events/IEventList.cs
@@ -1,1 +1,24 @@
-// TODO: Define Events list interface to be consumed by the main Program 
+using System;
+using System.Collections.Generic;
+
+namespace Events
+{
+    /// <summary>
+    /// Interface used to access the immutable Events Store
+    /// </summary>
+    public interface IEventList
+    {
+        /// <summary>
+        /// Stores an event in events list
+        /// </summary>
+        /// <param name="cashEvent"></param>
+        /// <returns>Key to newly stored event</returns>
+        IEventKey Store(CashEvent cashEvent);
+        /// <summary>
+        /// Searches for one or more events given an event key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        List<CashEvent> Find(IEventKey key);
+    }
+}
